6035691bd8c914716353ef77a00f95095391c13c
Sanskar Gouchandra <gouchandrasanskar.golu123@gmail.com>
2023-03-30 08:51:16 +0530

    second commit

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
+
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import tw from "tailwind-react-native-classnames";
+import { selectOrigin, selectDestination, setTravelTimeInformation } from "../slices/navSlice";
+import MapViewDirections from "react-native-maps-directions";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
 const Map = () => {
 const origin = useSelector(selectOrigin);
+const destination = useSelector(selectDestination);
+const mapRef = useRef(null);
+
+
+useEffect(()=>{
+  if (!origin || !destination) return;
+
+  mapRef.current.fitToSuppliedMakers(["origin", "destination"],{
+    edgePadding: { top: 50, right: 50, bottom: 50, left:50 },
+  });
+}, [origin , destination]);
 
   return (
     <MapView
@@ -18,6 +36,18 @@ const origin = useSelector(selectOrigin);
       longitudeDelta: 0.005,
     }}
   >
+
+{origin && destination && (
+                <MapViewDirections
+                    origin={origin.description}
+                    destination={destination.description}
+                    lineDashPattern={[1]}
+                    apikey={GOOGLE_MAPS_APIKEY}
+                    strokeWidth={3}
+                    strokeColor="black"
+                />
+            )}
+
         {origin?.location &&(
             <Marker
             coordinate={{
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,62 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'tailwind-react-native-classnames'
+import {GOOGLE_MAPS_APIKEY} from '@env'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style = {tw `bg-white flex-1`}>
+      <Text style={tw `text-center py-5 text-xl`}>Good Moarning, Guys</Text>
+      <View style ={tw `border-t border-gray-200 flex-shrink`}>
+        <View>
+            <GooglePlacesAutocomplete 
+           placeholder="Where to?"
+           styles={toInputBoxStyles}
+           fetchDetails={true}
+           returnKeyType={"search"}
+           minLength={2}
+           onPress={(data, details) => {
+            dispatch(
+                setDestination({
+                    location: details.geometry.location,
+                    description: data.description,
+                })
+            );
+            navigation.navigate("RideOptionsCard");
+        }}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en"
+            }}
+            
+            nearbyPlacesAPI='GooglePlaceSearch'
+            debounce={400}
+            />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default NavigateCard
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInputContainer:{
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+})
